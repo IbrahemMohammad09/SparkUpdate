81e3857ea41aa40b99240b51963b7281200955fd
adeeeb <adeebmoualla3@gmail.com>
2025-05-25 17:31:18 +0300

    Edit HeroSection , OurTopServicesSection and Create ProjectSection

--- Spark/src/Pages/HomePage.jsx
+++ Spark/src/Pages/HomePage.jsx
@@ -4,13 +4,15 @@ import NavigationBar from "../Components/NavigationBar";
 import MainTitle from "../Components/MainTitle";
 import HeroSection from "../Sections/HeroSection";
 import TopServices from "../Sections/TopServices";
+import OurProjects from "../Sections/OurProjects";
 function Homepage() {
   MainTitle("Home Page");
   return (
     <>
-      <NavigationBar /> 
+      <NavigationBar />
       <HeroSection />
       <TopServices />
+      <OurProjects />
       <Footer />
     </>
   );
